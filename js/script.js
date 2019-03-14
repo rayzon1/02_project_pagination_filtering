f6d28547b775e605c70280c48bcf8db7738cef50
Gerardo Keys <jotojry@icloud.com>
2019-03-13 22:33:14 -0700

    03/13 at home changes made

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const studentListChildren = document.querySelector('ul.student-list').children;
 const page = document.querySelector('div.page');
 
+// This function creates the pages with a max of 10 items per list.
 function showPage(list, page) {
    let end = page * 10;
    let start = end - 10;
@@ -13,24 +14,24 @@ function showPage(list, page) {
    }
 }
 
-function createLi(elem, attr, name) {
+// This function creates the element and sets the attribute to the arguments given.
+function createEl(elem, attr, name) {
    let element = document.createElement(elem);
    element.setAttribute(attr, name);
    return element;
 }
 
-const div = createLi('div', 'class', 'pagination');
-const ul = createLi('div', 'class', 'pages');
+const div = createEl('div', 'class', 'pagination');
+const ul = createEl('div', 'class', 'pages');
 div.appendChild(ul);
 page.appendChild(div);
-console.log(div);
 
 // Need to dynamically add links based on amount of people on the list.
 // CreateLi function within the appendPageLinks function is to create elements used in the loop.
 function appendPageLinks(num){
    for (let i = 1; i <= num; i ++){
-      let li = createLi('li', 'class', `${i}`);
-      let a = createLi('a', 'href', '#');
+      let li = createEl('li', 'class', 'link');
+      let a = createEl('a', 'href', '#');
       a.textContent = i;
       li.appendChild(a);
       ul.appendChild(li);
