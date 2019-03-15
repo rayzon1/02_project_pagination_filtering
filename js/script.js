ad305f6cf84866909566d4c120117fbdb01bf95e
Gerardo Keys <jotojry@icloud.com>
2019-03-14 17:49:42 -0700

    03/14 at work changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const studentListChildren = document.querySelector('ul.student-list').children;
 const page = document.querySelector('div.page');
+const pageHeader = document.querySelector('div.page-header');
+const div = createEl('div', 'class', 'pagination');
+const ul = createEl('ul', 'class', 'pages');
+
+div.appendChild(ul);
+page.appendChild(div);
 
 // This function creates the pages with a max of 10 items per list.
 function showPage(list, page) {
@@ -21,28 +27,50 @@ function createEl(elem, attr, name) {
    return element;
 }
 
-const div = createEl('div', 'class', 'pagination');
-const ul = createEl('div', 'class', 'pages');
-div.appendChild(ul);
-page.appendChild(div);
-
 // Need to dynamically add links based on amount of people on the list.
 // CreateLi function within the appendPageLinks function is to create elements used in the loop.
 function appendPageLinks(num){
    for (let i = 1; i <= num; i ++){
       let li = createEl('li', 'class', 'link');
       let a = createEl('a', 'href', '#');
+      a.setAttribute('class', 'active');
       a.textContent = i;
       li.appendChild(a);
       ul.appendChild(li);
+      a.addEventListener('click',  (e) => {
+         showPage(studentListChildren, e.target.textContent);
+      });
    }
 }
 
-// This event listener is for the created ul of numbered pagination links
-ul.addEventListener('click',  (e) => {
-   showPage(studentListChildren, e.target.textContent);
-});
+function inputSearch() {
+   
+   let divSearch = createEl('div', 'class', 'student-search');
+   let input = createEl('input', 'placeholder', 'Search for students...');
+   let button = createEl('button', 'class', 'button');
+   pageHeader.appendChild(divSearch);
+   divSearch.appendChild(input);
+   divSearch.appendChild(button);
+   button.textContent = 'Search';
+   input.setAttribute('class', 'input');
 
+   console.log(pageHeader);
+}
+
+function getSearch() {
+   const getInput = document.querySelector('input.input');
+   const getButton = document.querySelector('button.button');
+   console.log(getInput);
+   console.log(getButton);
+}
+
+// This event listener is for the created ul of numbered pagination links
 let number = Math.ceil(studentListChildren.length / 10);
 showPage(studentListChildren, 1);
 appendPageLinks(number);
+inputSearch();
+getSearch();
+
+
+
+
